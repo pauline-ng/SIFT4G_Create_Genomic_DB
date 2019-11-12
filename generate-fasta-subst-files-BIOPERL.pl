90de388504c1013fe0799e58bbcffe71584356be
pauline-ng <pauline_ng_98105@yahoo.com>
2019-11-12 10:45:47 +0800

    Update generate-fasta-subst-files-BIOPERL.pl

--- generate-fasta-subst-files-BIOPERL.pl
+++ generate-fasta-subst-files-BIOPERL.pl
@@ -23,8 +23,14 @@ use strict;
 use DBI;
 use Class::Struct;
 use Bio::DB::Fasta;
-require 'common-utils.pl';
-require 'dna_protein_subs.pl';
+#require 'common-utils.pl';
+#require 'dna_protein_subs.pl';
+use File::Basename;
+use Cwd qw(abs_path);
+my $directory_of_script = dirname(abs_path(__FILE__));
+require $directory_of_script . '/common-utils.pl';
+require $directory_of_script . '/dna_protein_subs.pl';
+
 #use lib "/mnt1/scripts_ensembl";
 #require 'DNA_PROT.pm';
 #use DNA_PROT qw (chr_is_mito);
